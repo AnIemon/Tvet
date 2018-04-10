4ba0bcca6b96790d3a16c92679720056387939ca
sze.chan11 <sze.chan11@600900J211DE418>
2018-04-10 17:59:43 +1000

    Try and add collision into game (didn't work).... yet

---
+++ Assets/~GoneHome/Scripts/LoadGames.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGames : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Goal")
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
