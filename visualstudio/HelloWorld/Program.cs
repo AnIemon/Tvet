51c35da0e0f090d7f230bb707da05a9269376e31
sze.chan11 <sze.chan11@600900J211DE418>
2018-05-01 17:48:29 +1000

    I made it print Hello World

---
+++ visualstudio/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World";
+
+            Console.WriteLine(message);
+            message = "10";
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+        
+    }
+
+}
