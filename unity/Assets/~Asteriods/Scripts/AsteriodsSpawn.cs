c6953035fbecd59003341eae6df1b1d2077a48c9
sze.chan11 <sze.chan11@600900J211DE418>
2018-05-08 17:51:18 +1000

    StartedToCreatedAsteriods

---
+++ unity/Assets/~Asteriods/Scripts/AsteriodsSpawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteriodsSpawn : MonoBehaviour
+{
+    public GameObject[] asteriodPrefabs;
+    public float spawnRate = 1f;
+    public float spawnRadius = 5f;
+    // Use this for initialization
+    void Spawn()
+    {
+        Vector3 rand = Random.insideUnitSphere * spawnRadius;
+        rand.z = 0f;
+        Vector3 position = transform.position + rand;
+        int randIndex = Random.Range(0, asteriodPrefabs.Length);
+        GameObject randAsteriod = asteriodPrefabs[randIndex];
+        GameObject clone = Instantiate(randAsteriod);
+        clone.transform.position = position;
+    }
+    void Start()
+    {
+        InvokeRepeating("Spawn", 0, spawnRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ unity/Assets/~Asteriods/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 20f;
+    public float rotationSpeed = 360f;
+    public Rigidbody2D rigid;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward, rotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward, rotationSpeed);
+        }
+    
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigid.AddForce(transform.up * speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigid.AddForce(-transform.up * speed);
+        }
+    }
+
+}
